7ee80bb5f8075049dd91956c30f596c9d909298a
Florian Schüller <florian.schueller@redhat.com>
2024-02-12 18:22:40 +0100

    blueprint-reference: add json examples for all chapters (COMPOSER-2148)
    Add json examples for all chapters where applicable.

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 // Define tabValues constant
 export const tabValues = [
-    { label: 'On premises', value: 'on-premises', attributes: { className: 'highlight tab on-premises' } },
-    { label: 'Hosted', value: 'hosted', attributes: { className: 'highlight tab hosted' } },
+    { label: '🔵 on premises', value: 'on-premises', attributes: { className: 'highlight tab on-premises' } },
+    { label: '🟤 hosted', value: 'hosted', attributes: { className: 'highlight tab hosted' } },
 ];
+
+export const tabValuesOnPremiseOnly = [
+    { label: '🔵 on premises', value: 'on-premises', attributes: { className: 'highlight tab on-premises' } },
+    { label: '⚪ hosted', value: 'hosted', attributes: { className: 'highlight tab disabled' } },
+];
+
+export const tabValuesHostedOnly = [
+    { label: '⚪ on premises', value: 'on-premises', attributes: { className: 'highlight tab disabled' } },
+    { label: '🟤 hosted', value: 'hosted', attributes: { className: 'highlight tab hosted' } },
+];
+
 
 //colorVars - see css/custom.css
 export default function Highlight({children, colorVar}) {
