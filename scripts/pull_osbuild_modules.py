7920d74e6f0bd19fa377394735dbfec8f20c0a2e
Simon de Vlieger <cmdr@supakeen.com>
2024-05-02 10:45:06 +0200

    auto: include osbuild sources
    Note that osbuild sources currently do not yet have the schemas stored
    separately and thus no files are generated. A PR will follow in osbuild
    to enable this.
    
    Signed-off-by: Simon de Vlieger <cmdr@supakeen.com>

--- scripts/pull_osbuild_modules.py
+++ scripts/pull_osbuild_modules.py
@@ -35,7 +35,6 @@ def escape(text):
 
 def main():
     root = pathlib.Path(__file__).parent.parent
-    dest = root / "docs/developer-guide/02-projects/osbuild/modules/stages"
 
     with tempfile.TemporaryDirectory() as tmp:
         path = pathlib.Path(tmp)
@@ -54,24 +53,32 @@ def main():
             ]
         )
 
-        for file in dest.glob("*.md"):
-            if file.name == "index.md":
-                continue
-            file.unlink()
-
-        for schema in repo.glob("stages/*.json"):
-            title = str(schema.name).removesuffix(".meta.json")
-            datas = json.loads(schema.read_text())
-
-            text = TEMPLATE.format(
-                title=title,
-                summary=escape(datas.get("summary", "")),
-                description=escape("\n".join(datas.get("description", [""]))),
-                schema_1=json.dumps(datas.get("schema", {}), indent=2),
-                schema_2=json.dumps(datas.get("schema_2", {}), indent=2),
+        for tiep in ["stages", "sources"]:
+            dest = (
+                root
+                / f"docs/developer-guide/02-projects/osbuild/modules/{tiep}"
             )
 
-            (dest / f"{title}.md").write_text(text)
+            for file in dest.glob("*.md"):
+                if file.name == "index.md":
+                    continue
+                file.unlink()
+
+            for schema in repo.glob(f"{tiep}/*.json"):
+                title = str(schema.name).removesuffix(".meta.json")
+                datas = json.loads(schema.read_text())
+
+                text = TEMPLATE.format(
+                    title=title,
+                    summary=escape(datas.get("summary", "")),
+                    description=escape(
+                        "\n".join(datas.get("description", [""]))
+                    ),
+                    schema_1=json.dumps(datas.get("schema", {}), indent=2),
+                    schema_2=json.dumps(datas.get("schema_2", {}), indent=2),
+                )
+
+                (dest / f"{title}.md").write_text(text)
 
     return 0
 
