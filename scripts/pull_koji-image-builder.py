e235f6c5031ee2772d2c21ff5c0a23fdc218d5ee
Simon de Vlieger <supakeen@redhat.com>
2025-08-05 15:04:36 +0200

    pull: `koji-image-builder`
    `koji-image-builder` recently gained documentation in its repository,
    let's include this on our website so it can be referenced.
    
    Signed-off-by: Simon de Vlieger <supakeen@redhat.com>

---
+++ scripts/pull_koji-image-builder.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import pathlib
+import subprocess
+import sys
+import shutil
+import tempfile
+import os
+
+from utils import patch_md
+
+def main():
+    root = pathlib.Path(__file__).parent.parent
+
+    base_url = "https://github.com/osbuild/koji-image-builder"
+    with tempfile.TemporaryDirectory() as tmp:
+        path = pathlib.Path(tmp)
+
+        repo = path / "repo"
+        repo.mkdir()
+
+        subprocess.run(
+            [
+                "git",
+                "clone",
+                "--depth",
+                "1",
+                base_url,
+                str(repo),
+            ]
+        )
+
+        dest = root / "docs/developer-guide/02-projects/koji-image-builder"
+
+        if dest.exists():
+            shutil.rmtree(dest)
+
+        shutil.copytree(
+            repo / "doc",
+            dest,
+        )
+
+        for root, _, files in os.walk(dest):
+            for file in files:
+                if file.endswith(".md"):
+                    md_file = pathlib.Path(root) / file
+                    originating_url = f"{base_url}/main/doc/{file}"
+                    patch_md(md_file, originating_url, relative_link_replacement=False)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
